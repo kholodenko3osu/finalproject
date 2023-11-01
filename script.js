893b391407c0562a3d000cc0a46cadd4c9390886
David A Kholodenko <kholodenko.3@osu.edu>
2023-11-01 19:54:54 -0400

    the thing

--- script.js
+++ script.js
@@ -1,7 +1,52 @@
-const shadow = document.querySelector('.shadow');
+let mouseX = 0;
+let mouseY = 0;
+let flashlight = document.getElementById("flashlight");
 
-document.addEventListener('mousemove', (e) => {
-  let x = e.clientX - (document.documentElement.clientWidth * 1.5);
-  let y = e.clientY - (document.documentElement.clientHeight * 1.5);
-  shadow.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-})
+//Detect touch device
+const isTouchDevice = () => {
+  try {
+    //We try to create TouchEvent(it would fail for desktops and throw error)
+    document.createEvent("TouchEvent");
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+function getMousePosition(e) {
+  mouseX = !isTouchDevice() ? e.pageX : e.touches[0].pageX;
+  mouseY = !isTouchDevice() ? e.pageY : e.touches[0].pageY;
+
+  flashlight.style.setProperty("--Xpos", mouseX + "px");
+  flashlight.style.setProperty("--Ypos", mouseY + "px");
+}
+
+document.addEventListener("mousemove", getMousePosition);
+document.addEventListener("touchmove", getMousePosition);
+
+// Passcode Protection - pass: absolution
+function protectpasscode() {
+  const result = document.getElementById("password").value;
+  let passprojects = "projects";
+  let passweekly = "weekly";
+  let passbaubles = "baubles";
+  let passabsolution = "absolution";
+  let space = '';
+  if (result == space) {
+     alert("Type what you can find.")
+  } 
+  else {
+     if (result == passprojects) {
+        window.location.replace("ravenTwo.html");
+     } else if (result == passweekly) {
+        window.location.replace("ravenTwo.html");
+     } else if (result == passbaubles) {
+      window.location.replace("ravenTwo.html");
+   } else if (result == passabsolution) {
+    window.location.replace("ravenTwo.html");
+ }   else {
+        alert("I cannot.");
+        location.reload();
+     }
+  }
+}
