61376af0adc125ae630cc5673ccccba0e431008f
David A Kholodenko <kholodenko.3@osu.edu>
2023-11-01 22:01:40 -0400

    Cohesion

--- script.js
+++ script.js
@@ -24,29 +24,35 @@ function getMousePosition(e) {
 document.addEventListener("mousemove", getMousePosition);
 document.addEventListener("touchmove", getMousePosition);
 
-// Passcode Protection - pass: absolution
+
+
+// Passcode Protection - passwords: projects, weekly, baubles, absolution. Stop sneaking around here!
 function protectpasscode() {
   const result = document.getElementById("password").value;
   let passprojects = "projects";
   let passweekly = "weekly";
   let passbaubles = "baubles";
   let passabsolution = "absolution";
+  let passozymandias = "ozymandias";
   let space = '';
   if (result == space) {
      alert("Type what you can find.")
   } 
   else {
      if (result == passprojects) {
-        window.location.replace("ravenTwo.html");
+        window.location.replace("MainProjects.html");
      } else if (result == passweekly) {
-        window.location.replace("ravenTwo.html");
+        window.location.replace("MainWeekly.html");
      } else if (result == passbaubles) {
       window.location.replace("ravenTwo.html");
    } else if (result == passabsolution) {
     window.location.replace("ravenTwo.html");
- }   else {
-        alert("I cannot.");
+ }   else if (result == passozymandias) {
+  window.location.replace("https://www.poetryfoundation.org/poems/46565/ozymandias", "_blank");
+}  else {
+        alert("I cannot. Try lowecase.");
         location.reload();
      }
   }
 }
+
